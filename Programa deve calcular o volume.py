3d44269dd7af238bafb634dc38bda04c9d195ad1
BloodyFenrir <156212260+BloodyFenrir@users.noreply.github.com>
2025-07-22 20:33:31 -0300

    Add files via upload

---
+++ Programa deve calcular o volume.py
@@ -0,0 +1,14 @@
+# Dimensões da caixa (em centímetros)
+comprimento = 12
+largura = 14
+altura = 20
+
+# Cálculo do volume
+volume = comprimento * largura * altura
+
+# Exibição das informações
+print("Dimensões da caixa:")
+print("Comprimento:", comprimento, "cm")
+print("Largura:", largura, "cm")
+print("Altura:", altura, "cm")
+print("Volume da caixa:", volume, "cm³")
